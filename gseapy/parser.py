910b2f85eb1366a45b8ffcac21c8e76a5f57ab8a
Zhuoqing Fang <fangzhuoqing@sibs.ac.cn>
2016-01-09 11:41:26 +0800

    update gseapy to 0.2.0

---
+++ gseapy/parser.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+def gsea_cls_parser(cls_path):
+    '''
+    extact sample name
+    '''
+    
+    with open(cls_path) as cls:
+        sample_name = cls.readlines()[1].split("_")
+    phenoPos = sample_name[1]
+    phenoNeg = sample_name[2]
+    return phenoPos,phenoNeg
+
+
+
+def gsea_edb_parser(results_path,index = 0,):
+   '''
+   paramter
+   -----------
+   results_path:
+   index:
+   
+   '''
+
+    
+    soup = BeautifulSoup(open(results_path),features='xml')
+    tag = soup.findAll('DTG')
+   
+    term = dict(tag[index].attrs)
+    # dict_keys(['RANKED_LIST', 'GENESET', 'FWER', 'ES_PROFILE', 
+    # 'HIT_INDICES', 'ES', 'NES', 'TEMPLATE', 'RND_ES', 'RANK_SCORE_AT_ES',
+    #'NP', 'RANK_AT_ES', 'FDR'])
+      
+    
+    enrich_term = term.get('GENESET').split("#")[1]
+    es_profile = term.get('ES_PROFILE').split(" ")
+    rank_es = term.get('RND_ES').split(" ")
+    hit_ind =term.get('HIT_INDICES').split(" ")
+
+    es_profile = [float(i) for i in es_profile ]
+    hit_ind = [float(i) for i in hit_ind ]
+    rank_es = [float(i) for i in rank_es ]
+    nes = term.get('NES')
+    pval = term.get('NP')
+    fdr =  term.get('FDR')
+    fwer = term.get('FWER')
+   
+   
+   
+    index_range = len(tag)-1
+    print("Index range: "+"0 - "+ str(index_range))
+    return enrich_term,es_profile,hit_ind, nes,pval,fdr,rank_es
+    
+
+def gsea_rank_metric(rank_path):
+    '''
+    parser rank_metric file
+    '''
+    
+    
+    rank_metric = pd.read_table(rank_path,header=None)
+    rank_metric.columns = ['gene_name','rank']
+    rank_metric['rank2'] = rank_metric['rank']
+     
+    return rank_metric
+    
+def gsea_gmt_parser(gene_set_path):
+    '''
+    parser gene set file
+    '''
+    
+    
+    with open(gene_set_path) as gene_sets:
+        gene_set_dict = { line.rstrip("\n").split("\t")[0]:  
+                          line.rstrip("\n").split("\t")[2:] 
+                          for line in gene_sets.readlines()}
+    return gene_set_dict
+    
---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='gseapy',
+      version='0.2.0',
+      description='Python wrapper around Gene Set Enrichment Analysis tool',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Software Development :: Libraries'],
+      keywords= ['GO', 'Gene Ontology', 'Biology', 'Enrichment',
+          'Bioinformatics', 'Computational Biology',
+          'library',],
+      url='https://github.com/BioNinja/gseapy',
+      author='Zhuoqing Fang',
+      author_email='fangzhuoqing@sibs.ac.cn',
+      license='MIT',
+      packages=['gseapy'],
+      
+      install_requires=[
+          'numpy>=1.6.0',
+          'pandas>=0.16',
+          'matplotlib>=1.4.3',
+          'beautifulsoup4>=4.4.1',],
+      entry_points={'console_scripts': ['gseapy = gseapy.__main__:main'],},
+      
+      zip_safe=False,
+      download_url='https://github.com/BioNinja/gseapy',)
+      
+__author__ = 'Zhuoqing Fang'
