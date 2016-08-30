e6ef4fd5a20d5ed3f38b21d7c5b8a13cbd56f382
Zhuoqing Fang <fangzhuoqing@sibs.ac.cn>
2016-08-30 21:59:46 +0800

    parsing gmt file bug fixed

--- gseapy/parser.py
+++ gseapy/parser.py
@@ -114,23 +114,26 @@ def gsea_gmt_parser(gmt, min_size = 3, max_size = 1000, gene_list=None):
             
     """
 
-    if gmt in get_libary_name():
-        import requests
-        ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/geneSetLibrary'
-        query_string = '?mode=text&libraryName=%s'
-        response = requests.get( ENRICHR_URL + query_string % gmt)
+    if gmt.lower().endswith(".gmt"):
+        with open(gmt) as genesets:    
+             genesets_dict = { line.strip("\n").split("\t")[0]: line.strip("\n").split("\t")[2:] 
+                              for line in genesets.readlines()}    
+    else:
+        if gmt in get_libary_name():
+            import requests
+            ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/geneSetLibrary'
+            query_string = '?mode=text&libraryName=%s'
+            response = requests.get( ENRICHR_URL + query_string % gmt)
+        else: 
+            raise Exception("gene_set files(.gmt) not found")
         if not response.ok:
             raise Exception('Error fetching enrichment results, check internet connection first.')
             
         print("Downloading and generating Enrichr library gene sets..............")            
-        genesets_dict = { line.decode().split("\t")[0]: [gene.split(",")[0] for gene in line.decode().split("\t")[2:-1]] 
+        genesets_dict = { line.decode().split("\t")[0]: 
+                          [gene.strip("\n").split(",")[0] for gene in line.decode().split("\t")[2:-1]] 
                           for line in response.iter_lines()}    
-
-    else:
-        with open(gmt) as genesets:    
-            genesets_dict = { line.split("\t")[0]: line.split("\t")[2:-1] 
-                              for line in genesets.readlines()}    
-
+ 
     
 
     #filtering dict
