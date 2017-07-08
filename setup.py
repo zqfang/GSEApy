28907aef747fbf1d8999d9821731d56522b4e305
Zhuoqing Fang <fztsing@126.com>
2017-07-08 11:52:45 +0800

    0.8.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import sys, os
 from setuptools import setup
 
-__version__='0.8.1'
+__version__='0.8.2'
 
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist bdist_wheel register upload")
