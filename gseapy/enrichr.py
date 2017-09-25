1d9e7d52677e4f12a261033f657457506b178d36
Zhuoqing Fang <fztsing@126.com>
2017-09-25 10:10:01 +0800

    Series input for enrichr

--- gseapy/enrichr.py
+++ gseapy/enrichr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# python 
+# python
 # see: http://amp.pharm.mssm.edu/Enrichr/help#api for API docs
 
 import sys, json, os, logging
@@ -37,7 +37,7 @@ class Enrichr(GSEAbase):
             genes = [str(gene) for gene in self.gene_list]
             genes_str = '\n'.join(genes)
 
-        elif isinstance(self.gene_list, (DataFrame, Series)):
+        elif isinstance(self.gene_list, DataFrame):
             #input type is bed file
             if self.gene_list.shape[1] >=3:
                 genes= self.gene_list.iloc[:,:3].apply(lambda x: "\t".join([str(i) for i in x]), axis=1).tolist()
@@ -45,15 +45,15 @@ class Enrichr(GSEAbase):
             elif self.gene_list.shape[1] == 2:
                genes= self.gene_list.apply(lambda x: ",".join([str(i) for i in x]), axis=1).tolist()
             else:
-               genes = self.gene_list.squeeze().tolist()
-            genes_str = '\n'.join(genes)
-
+               genes = self.gene_list.squeeze().tolist()            
+        elif isinstance(self.gene_list, Series):
+            genes = self.gene_list.squeeze().tolist()
         else:
             # get gene lists or bed file, or gene list with weighted values.
             with open(self.gene_list) as f:
                 genes = f.read()
-        
-            genes_str = str(genes) 
+
+        genes_str = str(genes)
 
         return genes_str
 
@@ -61,19 +61,19 @@ class Enrichr(GSEAbase):
         """run enrichr"""
 
         mkdirs(self.outdir)
-        
+
         #read input file
         genes_str=self.parse_input()
 
         # name of analysis or list
         description = str(self.descriptions)
-        
+
         #library validaty confirmationi
-        gene_set = str(self.gene_sets) 
+        gene_set = str(self.gene_sets)
         #logging start
-        logger = self._log_init(module=self.module,  
+        logger = self._log_init(module=self.module,
                                 log_level=logging.INFO if self.verbose else logging.WARNING)
-    
+
         logger.info("Connecting to Enrichr Server to get latest library names")
         if gene_set in DEFAULT_LIBRARY:
             enrichr_library = DEFAULT_LIBRARY
@@ -83,7 +83,7 @@ class Enrichr(GSEAbase):
                 sys.stderr.write("%s is not a enrichr library name\n"%gene_set)
                 sys.stdout.write("Hint: use get_library_name() to veiw full list of supported names")
                 sys.exit(1)
-            
+
         logger.info('Analysis name: %s, Enrichr Library: %s'%(description, gene_set))
 
         ## enrichr url
@@ -92,7 +92,7 @@ class Enrichr(GSEAbase):
         payload = {
           'list': (None, genes_str),
           'description': (None, description)
-           }   
+           }
         # response
         response = requests.post(ENRICHR_URL, files=payload)
         if not response.ok:
@@ -101,7 +101,7 @@ class Enrichr(GSEAbase):
         sleep(1)
         job_id = json.loads(response.text)
 
-        logger.debug('Job ID:'+ str(job_id))   
+        logger.debug('Job ID:'+ str(job_id))
         ENRICHR_URL_A = 'http://amp.pharm.mssm.edu/Enrichr/view?userListId=%s'
         user_list_id = job_id['userListId']
         response_gene_list = requests.get(ENRICHR_URL_A % str(user_list_id), timeout=None)
@@ -124,19 +124,19 @@ class Enrichr(GSEAbase):
         ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/export'
         query_string = '?userListId=%s&filename=%s&backgroundType=%s'
         user_list_id = str(job_id['userListId'])
-        filename = "%s.%s.%s.reports"%(gene_set, description, self.module)        
+        filename = "%s.%s.%s.reports"%(gene_set, description, self.module)
         url = ENRICHR_URL + query_string % (user_list_id, filename, gene_set)
 
-        # set max retries num =5 
+        # set max retries num =5
         s = requests.Session()
         retries = Retry(total=5, backoff_factor=0.1,
                         status_forcelist=[ 500, 502, 503, 504 ])
         s.mount('http://', HTTPAdapter(max_retries=retries))
         response = s.get(url, stream=True, timeout=None)
-        
-        logger.info('Downloading file of enrichment results: Job Id:'+ str(job_id)) 
+
+        logger.info('Downloading file of enrichment results: Job Id:'+ str(job_id))
         outfile="%s/%s.%s.%s.reports.txt"%(self.outdir, gene_set, description, self.module)
-        
+
         with open(outfile, 'wb') as f:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
@@ -149,15 +149,15 @@ class Enrichr(GSEAbase):
 
         #plotting
         if not self.__no_plot:
-            fig = barplot(df=df, cutoff=self.cutoff, 
+            fig = barplot(df=df, cutoff=self.cutoff,
                         figsize=self.figsize, top_term=self.__top_term,)
             if fig is None:
                 logger.warning("Warning: No enrich terms using library %s when cuttoff = %s"%(gene_set, self.cutoff))
             else:
                 fig.savefig(outfile.replace("txt", self.format),
                             bbox_inches='tight', dpi=300)
-        
-        return 
+
+        return
 def enrichr(gene_list, gene_sets, description='foo', outdir='Enrichr',
             cutoff=0.05, format='pdf', figsize=(8,6), top_term=10, no_plot=False, verbose=False):
     """Enrichr API.
@@ -179,5 +179,3 @@ def enrichr(gene_list, gene_sets, description='foo', outdir='Enrichr',
     enr.run()
 
     return enr
-
-    
