51ced935629e1060087946bdbad8400217223360
Zhuoqing Fang <fztsing@126.com>
2017-11-26 15:20:53 +0800

    doctest

--- condatest.sh
+++ condatest.sh
@@ -48,13 +48,14 @@ source deactivate
 conda env list | grep -q $name && conda env remove -y -n $name
 conda create -y -n $name  python=${PY_VERSION} \
     --file "requirements.txt" \
-    --file "test-requirements.txt"
+    --file "test-requirements.txt" \
     --file "docs/docs-requirements.txt"
 
 source activate $name
 
 conda install coverage
 conda install -c conda-forge coveralls
+
 
 log "run command test"
 #python setup.py test
