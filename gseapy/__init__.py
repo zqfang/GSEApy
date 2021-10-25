ed83dd2cccdf48b5c50d22a1b6947b083f6ef94f
zqfang <fangzq@stanford.edu>
2021-10-25 16:31:15 -0700

    bump version

--- gseapy/__init__.py
+++ gseapy/__init__.py
@@ -4,3 +4,8 @@ from .enrichr import enrichr
 from .parser import get_library_name
 from .plot import dotplot, barplot, heatmap, gseaplot
 from .__main__ import __version__
+
+
+__all__ = ['dotplot','barplot','heatmap','gseaplot', 
+           'replot','prerank','gsea','ssgsea',
+           'enrichr','get_library_name']
