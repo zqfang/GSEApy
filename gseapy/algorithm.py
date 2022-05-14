916dfb5397e43ac4bd112c1d583af317ad032309
Pearce Kieser <5055971+Pearcekieser@users.noreply.github.com>
2022-05-14 15:51:02 -0400

    Fix t-test

--- gseapy/algorithm.py
+++ gseapy/algorithm.py
@@ -370,8 +370,10 @@ def ranking_metric(df, method, pos, neg, classes, ascending):
     # exclude any zero stds.
     df_mean = df.groupby(by=classes, axis=1).mean()
     df_std = df.groupby(by=classes, axis=1).std()
-    n_pos = np.sum(classes == pos)
-    n_neg = np.sum(classes == neg)
+    class_values = np.array(list(classes.values()))
+    n_pos = np.sum(class_values == pos)
+    n_neg = np.sum(class_values == neg)
+
     if method in ["signal_to_noise", "s2n"]:
         ser = (df_mean[pos] - df_mean[neg]) / (df_std[pos] + df_std[neg])
     elif method in ["abs_signal_to_noise", "abs_s2n"]:
