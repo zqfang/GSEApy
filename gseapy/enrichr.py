624aec316574265c49341bd3bcaca03bf6202f8e
Zhuoqing Fang <fangzq@DN52egi1.SUNet>
2025-02-10 15:23:36 -0800

    use build-in json module explicity, #300

--- gseapy/enrichr.py
+++ gseapy/enrichr.py
@@ -291,7 +291,7 @@ class Enrichr(object):
         response = s.post(BGENR_URL, data=payload)
         if not response.ok:
             self._logger.error("Error fetching enrichment results: %s" % self._gs)
-        data = response.json()
+        data = json.loads(response.content)
         # Note: missig Overlap column
         colnames = [
             "Rank",
